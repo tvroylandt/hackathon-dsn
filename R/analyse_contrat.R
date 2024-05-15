d4e046da0666d13ebf9ce2c842706fac6da49b82
Thomas Vroylandt <tvroylandt@gmail.com>
2024-05-15 10:31:32 +0200

    Push export

--- R/analyse_contrat.R
+++ R/analyse_contrat.R
@@ -11,6 +11,9 @@ source("db.R")
 tbl_contrat <-
   tbl(con, Id(schema = "dadeh", table = "ddadtcontrat"))
 
+# table assure
+tbl_assure <- tbl(con, "ddadtassure")
+
 # table employeur assure
 tbl_assure_employeur <- tbl(con, "ddadtemployeur_assure")
 
@@ -28,9 +31,13 @@ tbl_contrat_join <- tbl_contrat |>
   mutate(is_boeth_contrat = if_else(!is.na(statut_boeth), 1, 0)) |>
   group_by(id_assure) |>
   mutate(is_boeth_assure = max(is_boeth_contrat)) |>
-  ungroup() |>
+  ungroup()
+
+# table avec infos assures
+tbl_contrat_join_demo <- tbl_contrat_join |>
+  left_join(tbl_assure, by = join_by(id_assure == id)) |>
   # calcul de l'âge
-  mutate(age = 2022 - as.numeric(substr(date_naissance, -4, -1)))
+  mutate(age = 2022 - as.numeric(substr(date_naissance, 5, 8)))
 
 # jointure arret travail
 tbl_arret_travail_join <- tbl_contrat_join |>
@@ -46,7 +53,7 @@ tbl_penibilite_join <- tbl_contrat_join |>
 
 ## Démographie ------------------------------------------------------------
 # sexe
-df_sexe <- tbl_contrat_join |>
+df_sexe <- tbl_contrat_join_demo |>
   mutate(sexe_recode = substr(num_immatriculation, 1, 1)) |>
   distinct(id_assure, is_boeth_assure, sexe_recode) |>
   group_by(sexe_recode) |>
@@ -58,8 +65,9 @@ df_sexe <- tbl_contrat_join |>
   collect()
 
 # âge
-df_age <- tbl_contrat_join |>
-  distinct(id_assure, is_boeth_assure, sexe_recode) |>
+df_age <- tbl_contrat_join_demo |>
+  left_join(tbl_assure, by = join_by(id_assure == id)) |>
+  distinct(id_assure, is_boeth_assure, age) |>
   group_by(age) |>
   summarise(
     n = n(),
@@ -103,8 +111,8 @@ df_tps_travail <- tbl_contrat_join |>
 
 ## PCS ---------------------------------------------------------------------
 df_pcs <- tbl_contrat_join |>
-  mutate(pcs = substr(pcs_ese, 1, 2))
-group_by(pcs) |>
+  mutate(pcs = substr(pcs_ese, 1, 2)) |>
+  group_by(pcs) |>
   summarise(
     n_contrat = n(),
     n_contrat_boeth_assure = sum(is_boeth_assure),
@@ -117,24 +125,48 @@ group_by(pcs) |>
 # on somme par individu
 # puis on cape à 365
 # et on calcul le nombre jours d'inactivité
-tbl_contrat_join_2022 <- tbl_contrat_join |>
+tbl_contrat_join_2022 <- tbl_contrat_join_demo |>
   filter(year(date_fin_contrat) >= 2022 &
            year(date_debut_contrat) <= 2022) |>
   # sinon date_diff ?
   mutate(
-    duree = difftime(date_fin_contrat, date_debut_contrat, units = "days"),
+    date_fin_contrat = if_else(
+      year(date_fin_contrat) > 2022,
+      as.Date("2022-12-31"),
+      date_fin_contrat
+    ),
+    date_debut_contrat = if_else(
+      year(date_debut_contrat) < 2022,
+      as.Date("2022-01-01"),
+      date_debut_contrat
+    ),
+    duree = difftime(date_debut_contrat, date_fin_contrat, units = "days"),
     duree = as.numeric(duree) + 1
   ) |>
-  group_by(id_assure, is_boeth_assure) |>
-  summarise(n_jours_couverts_ontrats = sum(duree),
+  group_by(id_assure, is_boeth_assure, age) |>
+  summarise(n_jours_couverts_contrats = sum(duree),
             .groups = "drop") |>
-  mutate(duree_inactivite = 365 - min(n_jours_couverts_ontrats, 365))
+  mutate(
+    n_jours_couverts_contrats = if_else(
+      n_jours_couverts_contrats > 365,
+      365,
+      n_jours_couverts_contrats
+    ),
+    duree_inactivite = 365 - n_jours_couverts_contrats
+  )
 
 # on collect par age
 df_age_jours_couverts <- tbl_contrat_join_2022 |>
   count(is_boeth_assure, age, duree_inactivite) |>
   collect() |>
-  mutate(groupe_age = cut(age, breaks = c(0, 25, 50, 100))) |>
+  mutate(
+    groupe_age = cut(age, breaks = c(0, 25, 50, 100)),
+    duree_inactivite_d = cut(
+      duree_inactivite,
+      breaks = c(-1, 0, 0.25 * 365, 0.50 * 365, 0.75 * 365, 1000),
+      right = TRUE
+    )
+  ) |>
   filter(!is.na(groupe_age))
 
 ## Passage CDD-CDI --------------------------------------------------------
@@ -157,8 +189,8 @@ df_contrat_typ_diff_sur_boeth <- tbl_contrat_join |>
 df_contrat_diff_sur_boeth <- tbl_contrat_join |>
   group_by(id_assure) |>
   summarise(
-    is_boeth_max = max(boeth),
-    is_boeth_min = min(boeth),
+    is_boeth_max = max(is_boeth_contrat),
+    is_boeth_min = min(is_boeth_contrat),
     .groups = "drop"
   ) |>
   count(is_boeth_max, is_boeth_min) |>
@@ -167,7 +199,7 @@ df_contrat_diff_sur_boeth <- tbl_contrat_join |>
 ## Arrêts de travail pour motif accident ----------------------------------
 df_arret_travail <- tbl_arret_travail_join |>
   filter(motif_arret %in% c("06")) |>
-  distinct(id_assure) |>
+  distinct(id_assure, is_boeth_assure) |>
   count(is_boeth_assure) |>
   collect()
 
@@ -182,3 +214,19 @@ df_penibilite <- tbl_penibilite_join |>
   collect()
 
 # Export ------------------------------------------------------------------
+writexl::write_xlsx(
+  list(
+    "sexe" = df_sexe,
+    "age" = df_age,
+    "nature_contrat" = df_nature_contrat,
+    "motif_rupture" = df_motif_rupture_cdi,
+    "quotite" = df_tps_travail,
+    "pcs" = df_pcs,
+    "jours_travailles" = df_age_jours_couverts,
+    "contrat_cdi_cdd" = df_contrat_typ_diff_sur_boeth,
+    "chgmt_boeth" = df_contrat_diff_sur_boeth,
+    "arret_travail" = df_arret_travail,
+    "penibilite" = df_penibilite
+  ),
+  "data/sorties.xlsx"
+)
